8929d91d1e63fdbc1a98f529b91f476894e3bd82
madan046 <madangowda046@gmail.com>
2022-11-25 18:45:13 +0530

    navbar

--- lab24/src/app/app-routing.module.ts
+++ lab24/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BookcartComponent } from './bookcart/bookcart.component';
+import { LoginComponent } from './login/login.component';
+import { MyorderComponent } from './myorder/myorder.component';
 
-const routes: Routes = [];
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { WhishlistComponent } from './whishlist/whishlist.component';
+
+const routes: Routes = [
+  {path:'',component:BookcartComponent},
+  {path:'bookcart',component:BookcartComponent},
+  {path:'wishlist',component:WhishlistComponent},
+  {path:'shopping-cart',component:ShoppingCartComponent},
+  {path:'myorder',component:MyorderComponent  },
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lab24/src/app/app.module.ts
+++ lab24/src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { WhishlistComponent } from './whishlist/whishlist.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BookcartComponent } from './bookcart/bookcart.component';
+import { MyorderComponent } from './myorder/myorder.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WhishlistComponent,
+    ShoppingCartComponent,
+    NavbarComponent,
+    BookcartComponent,
+    MyorderComponent,
+    LoginComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,HttpClientModule,FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
